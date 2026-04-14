1375814b9d83431df3ca84b5b8d44a3dc552fb90
Vladislav Bogomolov <vladislav.bogomolov@eltex.loc>
2026-04-14 17:58:21 +0700

    fix: fix properties, add controller for test

--- src/main/java/com/bvm/flakyhunter/controllers/IngestController.java
+++ src/main/java/com/bvm/flakyhunter/controllers/IngestController.java
@@ -1,5 +1,8 @@
 package com.bvm.flakyhunter.controllers;
 
+import com.bvm.flakyhunter.models.entities.Project;
+import com.bvm.flakyhunter.services.ProjectService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 public class IngestController {
+
+    private final ProjectService projectService;
 
     @PostMapping("/api/ingest")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
 
         return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping("/api/create")
+    public ResponseEntity<Project> createProject(@RequestParam String name) {
+        Project p = projectService.createProject(name);
+        return ResponseEntity.ok().body(p);
     }
 }
